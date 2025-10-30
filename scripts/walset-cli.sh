313a43725f0938680e44bece863a3a9033504829
eitankushnir <etn121415@gmail.com>
2025-10-30 14:23:09 +0200

    added path change to make it work with hyprpaper

--- scripts/walset-cli.sh
+++ scripts/walset-cli.sh
@@ -11,28 +11,23 @@ WALLPAPER="$1"
 # Validate file
 if [ ! -f "$WALLPAPER" ]; then
     echo "File does not exist: $WALLPAPER"
+    notify-send ":("
     exit 2
 fi
 
 # Set wallpaper using hyprpaper
 echo "Setting wallpaper..."
+export PATH="$HOME/.cargo/bin:$PATH"
 echo "preload = $WALLPAPER" > ~/.config/hypr/hyprpaper.conf
 echo "wallpaper = ,$WALLPAPER" >> ~/.config/hypr/hyprpaper.conf
 killall hyprpaper 2>/dev/null
 hyprpaper &
 
-#Generate pywal colors
-wal -i "$WALLPAPER" -n -s -t -e
-
 # Generate theme with matugen
 echo "Generating colors with matugen..."
 matugen image $WALLPAPER -m "dark"
-
 # Reload Waybar
 pkill waybar && waybar &
-
-# nvim
-killall -SIGUSR1 nvim
 
 # Reload Hyprland config (applies new color accents, if used)
 hyprctl reload
