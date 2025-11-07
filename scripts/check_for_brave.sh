7ea51963a5165440d8e1bd6b84a2de00795ed008
eitankushnir <etn121415@gmail.com>
2025-11-07 14:38:25 +0200

    moved all scripts to bin folder + matugen template cleanup

--- scripts/check_for_brave.sh
+++
@@ -1,7 +0,0 @@
-if pgrep -x brave > /dev/null; then
-    echo "1" > /tmp/brave_was_running
-    pkill brave
-else
-    echo "0" > /tmp/brave_was_running
-fi
-
--- waybar/.config/waybar/reload.sh
+++
@@ -1,2 +0,0 @@
-killall waybar
-waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css
