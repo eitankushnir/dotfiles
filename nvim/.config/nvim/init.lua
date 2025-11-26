10824421bb0c8155acc4b90807f5b2ad06be7a1f
eitankushnir <etn121415@gmail.com>
2025-11-27 00:53:27 +0200

    fix: ts context transparency fix

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,5 +9,6 @@ require("config.lazy")
 
 vim.cmd [[ colorscheme kanagawa ]]
 vim.cmd [[ TransparentEnable ]]
-vim.cmd [[ hi TreesitterContextBottom cterm=underline ]]
+vim.cmd [[ hi TreesitterContextBottom gui=underline ]]
+vim.cmd [[ hi TreesitterContextLineNumber guibg=none ]]
 require("config.pixel-fixes")
